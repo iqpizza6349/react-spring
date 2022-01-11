cf51f0fc931025d943acd60d9d4bb7112c01fd24
iqpizza6349 <iqpizza6349@gmail.com>
2022-01-12 05:24:09 +0900

    ✅ React Proxy (localhost:8080) ✨ App.js (connect spring response result)

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+  // 요청 받은 정보를 담아줄 변수 선언
+  const [testStr, setTestStr] = useState('');
+
+  // 변수 초기화
+  function callBack(str) {
+    setTestStr(str);
+  }
+
+  // 첫 번째 렌더링을 마친 후 실행
+  useEffect(
+      () => {
+        axios({
+          url: '/home',
+          method: 'GET'
+        }).then((res) => {
+          callBack(res.data);
+        })
+      }, []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          {testStr}
+        </header>
+      </div>
   );
 }
 
